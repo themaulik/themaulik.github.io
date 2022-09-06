df4aa42b6ca0d7143ddb4b33c2c80ce7f937aed0
Maulik Goswami <maulik.work247@gmail.com>
2022-09-06 19:19:26 +0530

    Services and Other Minner changes are done

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,8 +32,8 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 const skillsContent = document.getElementsByClassName('skills__content'),
     skillsHeader = document.querySelectorAll('.skills__header')
 
-function toggleSkills(){
-    let itemClass =  this.parentNode.className
+function toggleSkills() {
+    let itemClass = this.parentNode.className
     for (i = 0; i < skillsContent.length; i++) {
         skillsContent[i].className = 'skills__content skills__close'
     }
@@ -41,13 +41,13 @@ function toggleSkills(){
         this.parentNode.className = 'skills__content skills__open'
     }
 }
-skillsHeader.forEach((el) =>{
+skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
 
 /*==================== QUALIFICATION TABS ====================*/
 const tabs = document.querySelectorAll('[data-target]'),
-    tabContents = document.querySelectorAll('[data-content]')  
+    tabContents = document.querySelectorAll('[data-content]')
 
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
@@ -63,11 +63,30 @@ tabs.forEach(tab => {
         })
         tab.classList.add('qualification__active')
     })
-}) 
+})
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
 
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+}) 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
 
